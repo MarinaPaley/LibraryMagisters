8e4dcc0cf24be59f4a89a80afd98ba3bf531e737
Marina Paley <marina_paley@mail.ru>
2021-11-09 11:17:09 +0300

    Тесты #2

---
+++ Library/ORM.Tests/BaseMap.cs
@@ -0,0 +1,43 @@
+﻿// <copyright file="AuthorTests.cs" company="Васильева Марина Алексеевна">
+// Copyright (c) Васильева Марина Алексеевна. All rights reserved.
+// </copyright>
+
+namespace ORM.Tests
+{
+    using NHibernate;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Базовый класс для тестирования маппингов.
+    /// </summary>
+    public class BaseMap
+    {
+        protected ISessionFactory SessionFactory { get; private set; }
+
+        protected ISession Session { get; private set; }
+
+        [OneTimeSetUp]
+        public void SetUpOnce()
+        {
+            this.SessionFactory = NHibernateConfigurator.GetSessionFactory(showSql: true);
+        }
+
+        [OneTimeTearDown]
+        public void Terminate()
+        {
+            this.SessionFactory?.Dispose();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.Session = this.SessionFactory.OpenSession();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            this.Session?.Dispose();
+        }
+    }
+}
---
+++ Library/ORM/NHibernateTestsConfigurator.cs
@@ -0,0 +1,72 @@
+﻿// <copyright file="NHibernateTestsConfigurator.cs" company="Васильева Марина Алексеевна">
+// Copyright (c) Васильева Марина Алексеевна. All rights reserved.
+// </copyright>
+
+using System;
+using FluentNHibernate.Cfg.Db;
+
+namespace ORM
+{
+    using System.Reflection;
+    using FluentNHibernate.Cfg;
+    using NHibernate;
+    using NHibernate.Cfg;
+    using NHibernate.Tool.hbm2ddl;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    class NHibernateTestsConfigurator
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        private static FluentConfiguration fluentConfiguration;
+
+        private static Configuration config;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="assembly"></param>
+        /// <param name="showSql"></param>
+        /// <returns></returns>
+        public static ISessionFactory GetSessionFactory(Assembly assembly = null, bool showSql = false)
+        {
+            return GetConfiguration(assembly ?? Assembly.GetExecutingAssembly(), showSql).BuildSessionFactory();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="assembly"></param>
+        /// <param name="showSql"></param>
+        /// <returns></returns>
+        private static FluentConfiguration GetConfiguration(Assembly assembly, bool showSql = false)
+        {
+            if (fluentConfiguration is null)
+            {
+                var databaseConfiguration = SQLiteConfiguration.Standard.InMemory();
+
+                if (showSql)
+                {
+                    databaseConfiguration = databaseConfiguration.ShowSql().FormatSql();
+                }
+
+                fluentConfiguration = Fluently.Configure()
+                    .Database(databaseConfiguration)
+                    .Mappings(m => m.FluentMappings.AddFromAssembly(assembly))
+                    .ExposeConfiguration(BuildSchema);
+            }
+
+            return fluentConfiguration;
+        }
+
+        public static void BuildSchema(Configuration configuration)
+        {
+            var session = GetSessionFactory().OpenSession();
+            new SchemaExport(configuration)
+                .Execute(true, true, false, session.Connection, Console.Out);
+        }
+    }
+}
